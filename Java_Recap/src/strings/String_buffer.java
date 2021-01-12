9f9c79685defac344ce65a934175518dbdb24ff9
mikaelbeat <ryynanenphm@gmail.com>
2021-01-12 21:18:26 +0200

    Update

--- Java_Recap/src/strings/String_buffer.java
+++ Java_Recap/src/strings/String_buffer.java
@@ -9,13 +9,30 @@ public class String_buffer {
 		name.concat(" Person");
 		System.out.println(name);
 		
-		// String can be changed
-		StringBuffer name2 = new StringBuffer("Hello");
-		name2.append(" World!");
-		System.out.println(name2);
+		// String can be changed using append
+		StringBuffer test1 = new StringBuffer("Hello");
+		test1.append(" World!");
+		System.out.println("Using append -> " + test1);
 		
-		// 
-
+		// Adding to string using insert
+		StringBuffer test3 = new StringBuffer("This");
+		test3.insert(4, " WORKS!");
+		System.out.println("Using insert -> " + test3);
+		
+		// Replacing part of the string
+		StringBuffer test4 = new StringBuffer("Demo");
+		test4.replace(0, 1, "T");
+		System.out.println("Using replace -> " + test4);
+		
+		// Delete part of the string
+		StringBuffer test5 = new StringBuffer("Sun is shining!");
+		test5.delete(7, 14);
+		System.out.println("Using delete -> " + test5);
+		
+		// Reverse string
+		StringBuffer test6 = new StringBuffer("Cold War");
+		test6.reverse();
+		System.out.println("Using reverse -> " + test6);
 	}
 
 }
